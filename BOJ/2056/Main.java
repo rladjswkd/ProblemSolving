bcf661207ffc486b2185f811d9eff3677d5787b1
rladjswkd <dlfmaqkrrudah@gmail.com>
2024-06-07 15:13:32 +0900

    feat(BOJ/2056): 다이나믹 프로그래밍

---
+++ BOJ/2056/Main.java
@@ -0,0 +1,135 @@
+
+/*
+ * 여러 작업을 동시에 처리할 수 있다는 점이 중요하다.
+ * 
+ * 각 작업을 시작 시각, 종료 시각 쌍으로 계산
+ * 
+ * dp인가..?
+ */
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static Reader r = new Reader();
+
+	public static void main(String[] args) throws Exception {
+		int preTaskCount, taskDuration, latestPreTaskEndTime, res = 0;
+		int[] taskEndTimes = new int[r.nextInt()];
+
+		for (int i = 0; i < taskEndTimes.length; i++) {
+			latestPreTaskEndTime = 0;
+			taskDuration = r.nextInt();
+			preTaskCount = r.nextInt();
+			while (preTaskCount-- > 0)
+				latestPreTaskEndTime = Math.max(latestPreTaskEndTime, taskEndTimes[r.nextInt() - 1]);
+			taskEndTimes[i] = latestPreTaskEndTime + taskDuration;
+			res = Math.max(taskEndTimes[i], res);
+		}
+		bw.append(String.valueOf(res)).append('\n');
+		bw.flush();
+		bw.close();
+	}
+}
+
+class Reader {
+	final int SIZE = 1 << 13;
+	byte[] buffer = new byte[SIZE];
+	int index, size;
+
+	String nextString() throws Exception {
+		StringBuilder sb = new StringBuilder();
+		byte c;
+		while ((c = read()) < 32) {
+			if (size < 0)
+				return "endLine";
+		}
+		do
+			sb.appendCodePoint(c);
+		while ((c = read()) >= 32); // SPACE 분리라면 >로, 줄당 분리라면 >=로
+		return sb.toString();
+	}
+
+	char nextChar() throws Exception {
+		byte c;
+		while ((c = read()) < 32)
+			; // SPACE 분리라면 <=로, SPACE 무시라면 <로
+		return (char) c;
+	}
+
+	int nextInt() throws Exception {
+		int n = 0;
+		byte c;
+		boolean isMinus = false;
+		while ((c = read()) <= 32) {
+			if (size < 0)
+				return -1;
+		}
+		if (c == 45) {
+			c = read();
+			isMinus = true;
+		}
+		do
+			n = (n << 3) + (n << 1) + (c & 15);
+		while (isNumber(c = read()));
+		return isMinus ? ~n + 1 : n;
+	}
+
+	long nextLong() throws Exception {
+		long n = 0;
+		byte c;
+		boolean isMinus = false;
+		while ((c = read()) <= 32)
+			;
+		if (c == 45) {
+			c = read();
+			isMinus = true;
+		}
+		do
+			n = (n << 3) + (n << 1) + (c & 15);
+		while (isNumber(c = read()));
+		return isMinus ? ~n + 1 : n;
+	}
+
+	double nextDouble() throws Exception {
+		double n = 0, div = 1;
+		byte c;
+		boolean isMinus = false;
+		while ((c = read()) <= 32) {
+			if (size < 0)
+				return -12345;
+		}
+		if (c == 45) {
+			c = read();
+			isMinus = true;
+		} else if (c == 46) {
+			c = read();
+		}
+		do
+			n = (n * 10) + (c & 15);
+		while (isNumber(c = read()));
+		if (c == 46) {
+			while (isNumber(c = read())) {
+				n += (c - 48) / (div *= 10);
+			}
+		}
+		return isMinus ? -n : n;
+	}
+
+	boolean isNumber(byte c) {
+		return 47 < c && c < 58;
+	}
+
+	boolean isAlphabet(byte c) {
+		return (64 < c && c < 91) || (96 < c && c < 123);
+	}
+
+	byte read() throws Exception {
+		if (index == size) {
+			size = System.in.read(buffer, index = 0, SIZE);
+			if (size < 0)
+				buffer[0] = -1;
+		}
+		return buffer[index++];
+	}
+}
