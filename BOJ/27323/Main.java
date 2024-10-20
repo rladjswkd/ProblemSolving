07146a0aacd0aadffd6464f2cab0d029a2e6d774
rladjswkd <dlfmaqkrrudah@gmail.com>
2024-10-20 23:54:54 +0900

    feat(BOJ/27323): 수학

---
+++ BOJ/27323/Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+	private static int read() throws IOException {
+		int n = System.in.read() & 15, c;
+
+		while (48 <= (c = System.in.read()) && c <= 57)
+			n = (n << 3) + (n << 1) + (c & 15);
+		return n;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(read() * read());
+	}
+}
