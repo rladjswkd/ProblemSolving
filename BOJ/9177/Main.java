0fcdd4f28cb4eb0ae65e576f12f52944cc4b7902
rladjswkd <dlfmaqkrrudah@gmail.com>
2024-12-05 14:54:55 +0900

    feat(BOJ/9177): 다이나믹 프로그래밍(바텀업 추가)

--- BOJ/9177/Main.java
+++ BOJ/9177/Main.java
@@ -1,11 +1,6 @@
 import java.io.IOException;
 
 public class Main {
-	private static int n, l1, l2, l3;
-	private static char[] w1, w2, w3;
-	private static StringBuilder sb;
-	private static final String DATA_SET = "Data set ", CENTER = ": ", YES = "yes\n", NO = "no\n";
-	private static int[][] dp;
 
 	private static int read() throws IOException {
 		int n = System.in.read() & 15, c;
@@ -15,43 +10,20 @@ public class Main {
 		return n;
 	}
 
-	private static int solve(int i1, int i2, int i3) {
-		if (i3 == l3)
-			return 1;
-		if (i1 == l1) {
-			while (i2 < l2 && w2[i2] == w3[i3]) {
-				i2++;
-				i3++;
-			}
-			return i3 == l3 ? 1 : -1;
-		}
-		if (i2 == l2) {
-			while (i1 < l1 && w1[i1] == w3[i3]) {
-				i1++;
-				i3++;
-			}
-			return i3 == l3 ? 1 : -1;
-		}
-		if (dp[i1][i2] != 0)
-			return dp[i1][i2];
-		if (i1 < l1 && w1[i1] == w3[i3])
-			if (solve(i1 + 1, i2, i3 + 1) == 1)
-				return dp[i1][i2] = 1;
-		if (i2 < l2 && w2[i2] == w3[i3])
-			if (solve(i1, i2 + 1, i3 + 1) == 1)
-				return dp[i1][i2] = 1;
-		return dp[i1][i2] = -1;
-	}
-
 	public static void main(String[] args) throws IOException {
-		int c;
+		int n, l1, l2, l3, c;
+		char[] w1, w2, w3;
+		boolean[][] dp;
+		StringBuilder sb;
+		final String DATA_SET = "Data set ", CENTER = ": ", YES = "yes\n", NO = "no\n";
 
 		n = read();
 		w1 = new char[200];
 		w2 = new char[200];
 		w3 = new char[400];
+		dp = new boolean[201][201];
 		sb = new StringBuilder();
-		for (int i = 1; i <= n; i++) {
+		for (int round = 1; round <= n; round++) {
 			l1 = l2 = l3 = 0;
 			while ((c = System.in.read()) > 32)
 				w1[l1++] = (char) c;
@@ -59,8 +31,15 @@ public class Main {
 				w2[l2++] = (char) c;
 			while ((c = System.in.read()) > 32)
 				w3[l3++] = (char) c;
-			dp = new int[l1][l2];
-			sb.append(DATA_SET).append(i).append(CENTER).append(solve(0, 0, 0) == 1 ? YES : NO);
+			dp[0][0] = true;
+			for (int j = 1; j <= l2; j++)
+				dp[0][j] = dp[0][j - 1] && w2[j - 1] == w3[j - 1];
+			for (int i = 1; i <= l1; i++) {
+				dp[i][0] = dp[i - 1][0] && w1[i - 1] == w3[i - 1];
+				for (int j = 1; j <= l2; j++)
+					dp[i][j] = (dp[i - 1][j] && w1[i - 1] == w3[i + j - 1]) || (dp[i][j - 1] && w2[j - 1] == w3[i + j - 1]);
+			}
+			sb.append(DATA_SET).append(round).append(CENTER).append(dp[l1][l2] ? YES : NO);
 		}
 		System.out.print(sb.toString());
 	}
