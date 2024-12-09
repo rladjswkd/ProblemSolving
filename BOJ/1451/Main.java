3fd4cf2c538e1f8ca9b52a3b32c09e398ad51c6a
rladjswkd <dlfmaqkrrudah@gmail.com>
2024-12-09 15:17:14 +0900

    feat(BOJ/1451): 누적합, 브루트포스

--- BOJ/1451/Main.java
+++ BOJ/1451/Main.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-	private static int h, w;
+	private static int height, width;
 	private static int[][] a;
 
 	private static int read() throws IOException {
@@ -13,25 +13,34 @@ public class Main {
 	}
 
 	public static void main(String[] args) throws IOException {
-		int r1, r2, r3;
-		long res = 0;
+		long res = 0, r1, r2, r3, r4;
 
-		a = new int[(h = read()) + 1][(w = read()) + 1];
-		for (int i = 1; i <= h; i++) {
-			for (int j = 1; j <= w; j++)
+		a = new int[(height = read()) + 1][(width = read()) + 1];
+		for (int i = 1; i <= height; i++) {
+			for (int j = 1; j <= width; j++)
 				a[i][j] = (System.in.read() & 15) + a[i - 1][j] + a[i][j - 1] - a[i - 1][j - 1];
 			System.in.read();
 		}
-		for (int h1 = 1; h1 < h; h1++) {
-			for (int h2 = h1 + 1; h2 < h; h2++) {
-				for (int v1 = 1; v1 < w; v1++) {
-					for (int v2 = v1 + 1; v2 < w; v2++) {
-						
-						res = Math.max(res, );
-					}
-				}
+		for (int v1 = 1; v1 < width - 1; v1++)
+			for (int v2 = v1 + 1; v2 < width; v2++)
+				res = Math.max(res,
+						(long) a[height][v1] * (long) (a[height][v2] - a[height][v1]) * (a[height][width] - a[height][v2]));
+		for (int h1 = 1; h1 < height - 1; h1++)
+			for (int h2 = h1 + 1; h2 < height; h2++)
+				res = Math.max(res,
+						(long) a[h1][width] * (long) (a[h2][width] - a[h1][width]) * (a[height][width] - a[h2][width]));
+		for (int h = 1; h < height; h++) {
+			for (int v = 1; v < width; v++) {
+				r1 = a[h][v];
+				r2 = a[h][width] - r1;
+				r4 = a[height][v] - r1;
+				r3 = a[height][width] - r2 - r4 - r1;
+				res = Math.max(res, (r1 + r2) * r3 * r4);
+				res = Math.max(res, (r2 + r3) * r1 * r4);
+				res = Math.max(res, (r3 + r4) * r1 * r2);
+				res = Math.max(res, (r4 + r1) * r2 * r3);
 			}
 		}
-		System.out.println();
+		System.out.println(res);
 	}
 }
