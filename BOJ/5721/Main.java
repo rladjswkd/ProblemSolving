f6497a132ab2c7734a52576aaf894d134e3caea9
rladjswkd <dlfmaqkrrudah@gmail.com>
2024-12-24 00:34:24 +0900

    feat(BOJ/5721): 최적화

--- BOJ/5721/Main.java
+++ BOJ/5721/Main.java
@@ -2,8 +2,7 @@ import java.io.IOException;
 
 public class Main {
 	private static int height, width;
-	private static int[][] boxes;
-	private static long[][] dp;
+	private static int[][] dp;
 	private static final int PADDING = 2;
 
 	private static int read() throws IOException {
@@ -16,18 +15,17 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		StringBuilder sb = new StringBuilder();
+		int[] a;
 
 		while ((height = read()) + (width = read()) != 0) {
-			boxes = new int[height][width];
-			for (int i = 0; i < height; i++)
-				for (int j = 0; j < width; j++)
-					boxes[i][j] = read();
-			dp = new long[height + PADDING][width + PADDING];
-			for (int i = 0; i < height; i++)
-				for (int j = 0; j < width; j++)
-					dp[i + PADDING][j + PADDING] = Math.max(dp[i + PADDING][j] + boxes[i][j], dp[i + PADDING][j + 1]);
-			for (int i = PADDING; i < height + PADDING; i++)
-				dp[i][width + 1] = Math.max(dp[i][width + 1] + dp[i - 2][width + 1], dp[i - 1][width + 1]);
+			dp = new int[height + PADDING][width + PADDING];
+			for (int i = 0; i < height; i++) {
+				a = dp[i + PADDING];
+				for (int j = 0; j < width - 1; j++)
+					a[j + PADDING] = Math.max(a[j] + read(), a[j + 1]);
+				a[width + 1] = Math.max(Math.max(a[width - 1] + read(), a[width]) + dp[i][width + 1],
+						dp[i + 1][width + 1]);
+			}
 			sb.append(dp[height + 1][width + 1]).append('\n');
 		}
 		System.out.print(sb.toString());
